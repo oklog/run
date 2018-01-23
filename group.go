7ddcde6bf983f2bda90fb431c0f5afd3189c6ac3
Aleksandr Chernetskiy <a_chernetskiy@wargaming.net>
2018-01-23 22:54:29 +0300

    Implement the sidecar actor.
    The sidecar actor does not interrupt other running actors, unless
    it returns an error.

--- group.go
+++ group.go
@@ -6,7 +6,8 @@
 package run
 
 // Group collects actors (functions) and runs them concurrently.
-// When one actor (function) returns, all actors are interrupted.
+// When one actor (function) returns, except for a sidecar actor
+// that does not return an error, all actors are interrupted.
 // The zero value of a Group is useful.
 type Group struct {
 	actors []actor
@@ -19,11 +20,30 @@ type Group struct {
 // The first actor (function) to return interrupts all running actors.
 // The error is passed to the interrupt functions, and is returned by Run.
 func (g *Group) Add(execute func() error, interrupt func(error)) {
-	g.actors = append(g.actors, actor{execute, interrupt})
+	g.actors = append(g.actors, actor{
+		execute:   execute,
+		interrupt: interrupt,
+		sidecar:   false,
+	})
+}
+
+// AddSidecar add a sidecar actor to the group. Each actor must satisfy
+// the same conditions as a normal actor from Add function.
+//
+// If the sidecar actor does not return an error, it does not interrupt
+// other running actors. Otherwise it acts like a normal actor.
+func (g *Group) AddSidecar(execute func() error, interrupt func(error)) {
+	g.actors = append(g.actors, actor{
+		execute:   execute,
+		interrupt: interrupt,
+		sidecar:   true,
+	})
 }
 
 // Run all actors (functions) concurrently.
-// When the first actor returns, all others are interrupted.
+// There are two cases when all actors will be interrupted.
+// The first case, when the first actor returns an error.
+// The second case, when the first normal actor returns.
 // Run only returns when all actors have exited.
 // Run returns the error returned by the first exiting actor.
 func (g *Group) Run() error {
@@ -32,24 +52,44 @@ func (g *Group) Run() error {
 	}
 
 	// Run each actor.
-	errors := make(chan error, len(g.actors))
+	actorExecutionResultChan := make(chan *actorExecutionResult, len(g.actors))
 	for _, a := range g.actors {
 		go func(a actor) {
-			errors <- a.execute()
+			actorExecutionResultChan <- &actorExecutionResult{
+				fromSidecar: a.sidecar,
+				err:         a.execute(),
+			}
 		}(a)
 	}
 
-	// Wait for the first actor to stop.
-	err := <-errors
+	// The original error.
+	var err error
+
+	// Number of actors that need to wait.
+	waitCount := len(g.actors)
+
+	// Wait for the first normal actor to stop
+	// or the sidecar actor returns an error.
+	for waitCount > 0 {
+		execResult := <-actorExecutionResultChan
+		if execResult.fromSidecar && execResult.err == nil {
+			// If sidecar actor stopped without error, then
+			// we do not need to wait for it to stop.
+			waitCount--
+		} else {
+			err = execResult.err
+			break
+		}
+	}
 
 	// Signal all actors to stop.
 	for _, a := range g.actors {
 		a.interrupt(err)
 	}
 
-	// Wait for all actors to stop.
-	for i := 1; i < cap(errors); i++ {
-		<-errors
+	// Wait for actors to stop.
+	for i := 1; i < waitCount; i++ {
+		<-actorExecutionResultChan
 	}
 
 	// Return the original error.
@@ -59,4 +99,10 @@ func (g *Group) Run() error {
 type actor struct {
 	execute   func() error
 	interrupt func(error)
+	sidecar   bool
+}
+
+type actorExecutionResult struct {
+	fromSidecar bool
+	err         error
 }
