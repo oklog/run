eff936c276df4821771d61a000ad658dd8f4ad85
achernetsky <alexander.chernetsky@gmail.com>
2018-01-23 22:54:29 +0300

    Implement the sidecar actor.
    The sidecar actor does not interrupt other running actors, unless
    it returns an error.

--- group.go
+++ group.go
@@ -6,8 +6,11 @@
 package run
 
 // Group collects actors (functions) and runs them concurrently.
-// When one actor (function) returns, all actors are interrupted.
-// The zero value of a Group is useful.
+// There are two types of actors (functions): regular and sidecar.
+// They differ in how their returns are handled. When a regular
+// actor (function) returns, all actors are interrupted unconditionally.
+// When a sidecar actor (function) exits, all actors are interrupted only
+// if an error is returned. The zero value of a Group is useful.
 type Group struct {
 	actors []actor
 }
@@ -19,37 +22,77 @@ type Group struct {
 // The first actor (function) to return interrupts all running actors.
 // The error is passed to the interrupt functions, and is returned by Run.
 func (g *Group) Add(execute func() error, interrupt func(error)) {
-	g.actors = append(g.actors, actor{execute, interrupt})
+	g.actors = append(g.actors, actor{
+		execute:   execute,
+		interrupt: interrupt,
+		sidecar:   false,
+	})
 }
 
-// Run all actors (functions) concurrently.
-// When the first actor returns, all others are interrupted.
-// Run only returns when all actors have exited.
-// Run returns the error returned by the first exiting actor.
+// Add a sidecar actor to the group. Each actor must meet the same
+// requirements as a regular actor as desribed in the documentation for
+// Add function.
+//
+// If a sidecar actor does not return an error, it does not interrupt
+// all running actors. Otherwise it acts like a regular actor.
+func (g *Group) AddSidecar(execute func() error, interrupt func(error)) {
+	g.actors = append(g.actors, actor{
+		execute:   execute,
+		interrupt: interrupt,
+		sidecar:   true,
+	})
+}
+
+// Run all actors (functions) concurrently. It interrupts all actors when
+// either a regular actor returns, or a sidecar actor returns an error.
+// Run only returns when all actors have exited. Run returns the error
+// returned by the first exiting actor.
 func (g *Group) Run() error {
 	if len(g.actors) == 0 {
 		return nil
 	}
 
 	// Run each actor.
-	errors := make(chan error, len(g.actors))
+	actorResults := make(chan actorResult, len(g.actors))
 	for _, a := range g.actors {
 		go func(a actor) {
-			errors <- a.execute()
+			actorResults <- actorResult{
+				sidecar: a.sidecar,
+				err:     a.execute(),
+			}
 		}(a)
 	}
 
-	// Wait for the first actor to stop.
-	err := <-errors
+	// An original error to return.
+	var err error
+
+	// A number of actors that need to be waited to return.
+	waitCount := len(g.actors)
+
+	// Wait till a) a regular actor finishes, or b) a sidecar actor
+	// returns an error, or c) all sidecar actors returno with no error, if
+	// there are no regular actors.
+	for waitCount > 0 {
+		result := <-actorResults
+		if result.sidecar && result.err == nil {
+			// If a sidecar actor stops without an error, then
+			// we do not need to wait for it to stop later.
+			waitCount--
+		} else {
+			// Remember the error to return from this function.
+			err = result.err
+			break
+		}
+	}
 
 	// Signal all actors to stop.
 	for _, a := range g.actors {
 		a.interrupt(err)
 	}
 
-	// Wait for all actors to stop.
-	for i := 1; i < cap(errors); i++ {
-		<-errors
+	// Wait for actors to stop.
+	for i := 1; i < waitCount; i++ {
+		<-actorResults
 	}
 
 	// Return the original error.
@@ -59,4 +102,10 @@ func (g *Group) Run() error {
 type actor struct {
 	execute   func() error
 	interrupt func(error)
+	sidecar   bool
+}
+
+type actorResult struct {
+	sidecar bool
+	err     error
 }
--- group_test.go
+++ group_test.go
@@ -38,10 +38,60 @@ func TestOne(t *testing.T) {
 	}
 }
 
+func TestOneSidecar(t *testing.T) {
+	var g run.Group
+	g.AddSidecar(func() error { return nil }, func(error) {})
+	res := make(chan error)
+	go func() { res <- g.Run() }()
+	select {
+	case err := <-res:
+		if err != nil {
+			t.Errorf("a nil expected, got error %v", err)
+		}
+	case <-time.After(100 * time.Millisecond):
+		t.Error("timeout")
+	}
+}
+
+func TestOneSidecarReturningError(t *testing.T) {
+	myError := errors.New("foobar")
+	var g run.Group
+	g.AddSidecar(func() error { return myError }, func(error) {})
+	res := make(chan error)
+	go func() { res <- g.Run() }()
+	select {
+	case err := <-res:
+		if want, have := myError, err; want != have {
+			t.Errorf("want %v, have %v", want, have)
+		}
+	case <-time.After(100 * time.Millisecond):
+		t.Error("timeout")
+	}
+}
+
 func TestMany(t *testing.T) {
 	interrupt := errors.New("interrupt")
 	var g run.Group
 	g.Add(func() error { return interrupt }, func(error) {})
+	cancel := make(chan struct{})
+	g.Add(func() error { <-cancel; return nil }, func(error) { close(cancel) })
+	g.AddSidecar(func() error { <-cancel; return nil }, func(error) {})
+	res := make(chan error)
+	go func() { res <- g.Run() }()
+	select {
+	case err := <-res:
+		if want, have := interrupt, err; want != have {
+			t.Errorf("want %v, have %v", want, have)
+		}
+	case <-time.After(100 * time.Millisecond):
+		t.Errorf("timeout")
+	}
+}
+
+func TestSidecarActorInterruptsOther(t *testing.T) {
+	interrupt := errors.New("interrupt")
+	var g run.Group
+	g.AddSidecar(func() error { return interrupt }, func(error) {})
 	cancel := make(chan struct{})
 	g.Add(func() error { <-cancel; return nil }, func(error) { close(cancel) })
 	res := make(chan error)
@@ -53,5 +103,20 @@ func TestMany(t *testing.T) {
 		}
 	case <-time.After(100 * time.Millisecond):
 		t.Errorf("timeout")
+	}
+}
+
+func TestSidecarActorDoesNotInterruptOther(t *testing.T) {
+	var g run.Group
+	g.AddSidecar(func() error { return nil }, func(error) {})
+	cancel := make(chan struct{})
+	g.Add(func() error { <-cancel; return nil }, func(error) { close(cancel) })
+	res := make(chan error)
+	go func() { res <- g.Run() }()
+	select {
+	case err := <-res:
+		t.Errorf("a nil expected, got error %v", err)
+	case <-time.After(100 * time.Millisecond):
+		break
 	}
 }
